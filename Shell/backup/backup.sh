ce241f62e62ea80b4f993bf6748da798e958ac23
JinnLynn <eatfishlin@gmail.com>
2012-06-03 21:34:21 +0800

    backup 可通过task.txt文件进行任务配置

--- Shell/backup/backup.sh
+++ Shell/backup/backup.sh
@@ -25,15 +25,18 @@ LOGDIR=./log
 #参数2: 备份的名称，用于在服务端建立文件夹
 #参数3: 备份NAS上的文件
 function single_backup() {
-	echo "$(date +"%H:%M:%S") $2 start"
-	echo "------------------------------------------------"
-	#检查目录是否已存在，如果没有建立目录
-	ssh $BHOST "if [ ! -d $BDST/$2 ]; then mkdir $BDST/$2 ; chown -R backup:users $BDST/$2 ; chmod -R 744 $BDST/$2 ; fi"
 	#被修改回删除的文件保存处理 每天生成一个目录
 	BDIR="$BDST/$2/$(date +0%u)"
 	OPTS="-av --force --ignore-errors --delete --backup --backup-dir=$BDIR"
 	SSH_OPT="ssh -i $SSHKEY"
 	EXCULDE_OPT="--exclude-from=$EXCLUDE"
+
+	echo "$(date +"%H:%M:%S") $2 start"
+	echo "------------------------------------------------"
+
+	#检查目录是否已存在，如果没有建立目录
+	$SSH_OPT $BHOST "[ -d $BDST/$2 ] || mkdir $BDST/$2" < /dev/null
+	
 	#清除旧的增量备份数据
 	rsync --delete -a $SCRIPTPATH/.emptydir/ $BHOST:$BDIR
 	#同步文件
@@ -56,35 +59,14 @@ function backup() {
 
 	echo "$(date +"%H:%M:%S") backup start\n"
 
-	#以下为备份列表 格式：backup 备份文件夹绝对路径 备份名
-	#注意：备份文件夹路径最好以'/'结束
-	#------------------------------------------------------------------------#
+	# 读取计划列表 开始备份
 
-
-	#JMBP
-	#备份iTunes
-	single_backup /Users/JinnLynn/Music/iTunes/ JMBP.iTunes
-
-	#备份Developer
-	single_backup /Users/JinnLynn/Developer/ JMBP.Developer
-
-	#备份Documents
-	single_backup /Users/JinnLynn/Documents/ JMBP.Documents
-
-	#备份Applications
-	single_backup /Applications/ JMBP.Applications
-
-	#备份Pictures
-	single_backup /Users/JinnLynn/Pictures/ JMBP.Pictures
-
-	#JMBPWin
-	single_backup /Volumes/BOOTCAMP/Users/JinnLynn/Developer/ JMBPWin.Developer
-
-	#JNAS SCMs
-	single_backup /volume1/DevCenter/SCMs/ JNAS.SCMs nas
-
-
-	#------------------------------------------------------------------------#
+	while read LINE
+	do
+		if [ "${LINE:0:1}" != "" -a "${LINE:0:1}" != "#" ]; then
+			single_backup $LINE 
+		fi
+	done < task.txt
 
 	#删除空目录
 	rmdir .emptydir
