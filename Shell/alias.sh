4ec27e0e3bc5abd2a9ba5e5480937731e35b8e5f
JinnLynn <eatfishlin@gmail.com>
2012-06-04 08:58:31 +0800

    添加SSH 切换路径相关别名

--- Shell/alias.sh
+++ Shell/alias.sh
@@ -1,6 +1,15 @@
 alias kits="kits.sh"
-alias env=". ~/.bashrc; echo 'rebuild env ok.'"
 alias ls="ls -h"
 alias ll="ls -l"
 
 alias subl="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
+
+#SSH相关
+alias ssh.home="ssh $JHOST"
+alias ssh.scm="ssh scm@$JHOST"
+alias ssh.work="ssh jinnlynn@172.16.5.14"
+alias ssh.github="ssh -T git@github.com"
+
+#文件夹
+alias to.kits="cd $KITS; pwd"
+alias to.shell="cd $KITSSHELL; pwd"
