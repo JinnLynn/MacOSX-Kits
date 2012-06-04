453f6e22af1d95fb550d209c41592a406ad89ee1
JinnLynn <eatfishlin@gmail.com>
2012-06-04 13:09:27 +0800

    隐藏文件的显示与否简化

--- Shell/filesystem.sh
+++ Shell/filesystem.sh
@@ -1,25 +1,17 @@
-# 显示隐藏的文件
+# 是否显示隐藏的文件
 function show_hidden_files() {
-  defaults write com.apple.Finder AppleShowAllFiles YES
-  osascript -e 'tell application "Finder" to quit'
-  sleep 1
-  osascript -e 'tell application "Finder" to activate'
-}
-
-# 隐藏隐藏的文件
-function hide_hidden_files() {
-  defaults write com.apple.Finder AppleShowAllFiles NO
-  osascript -e 'tell application "Finder" to quit'
-  sleep 1
-  osascript -e 'tell application "Finder" to activate'
+    defaults write com.apple.Finder AppleShowAllFiles $1
+    osascript -e 'tell application "Finder" to quit'
+    sleep 1
+    osascript -e 'tell application "Finder" to activate'
 }
 
 case $1 in
     'hiddenfiles' )     #隐藏文件的显示与隐藏
         if [ "$2" = 'show' ]; then
-            show_hidden_files
+                show_hidden_files YES
         elif [ "$2" = 'hide' ]; then
-            hide_hidden_files
+                show_hidden_files NO
         fi
         ;;
     * )
