1ccbb55b211716f448f02f4b4251ed688a574e9f
JinnLynn <eatfishlin@gmail.com>
2012-06-04 09:45:48 +0800

    kits 使用别名

--- Shell/alias.sh
+++ Shell/alias.sh
@@ -1,7 +1,8 @@
 alias kits="kits.sh"
-alias ls="ls -h"
-alias ll="ls -l"
 
+alias ll="ls -h -l"
+
+#sublime text 2的命令行模式
 alias subl="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
 
 #SSH相关
@@ -10,6 +11,22 @@ alias ssh.scm="ssh scm@$JHOST"
 alias ssh.work="ssh jinnlynn@172.16.5.14"
 alias ssh.github="ssh -T git@github.com"
 
-#文件夹
+#路径更改
 alias to.kits="cd $KITS; pwd"
 alias to.shell="cd $KITSSHELL; pwd"
+
+
+#KITS
+
+alias kits.backup="kits backup"
+alias kits.genpac="kits genpac"
+
+#itunes 需袋参数
+# $1 类型 lyric = 获取当前播放音乐的歌词 rate = 给当前播放的歌曲评级
+# $2 如果$1=rate才有效 1~5
+alias kits.itunes="kits itunes"
+
+#MAMP控制
+alias kits.mamp.start="kits mamp start"
+alias kits.mamp.stop="kits mamp stop"
+alias kits.mamp.restart="kits mamp restart"
