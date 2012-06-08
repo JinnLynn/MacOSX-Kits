300f83c26c54af32cead2bf66c5c2aceebf62287
JinnLynn <eatfishlin@gmail.com>
2012-06-08 12:16:27 +0800

    mamp.sh 优化

--- Shell/mamp.sh
+++ Shell/mamp.sh
@@ -4,31 +4,25 @@
 # MAMP相关控制
 ##
 
-res=""
 pushd /Applications/MAMP/bin/ > /dev/null
-if [[ "$1" = 'start' ]]; then
-    ./start.sh
-    res="MAMP started."
-elif [[ "$1" = 'stop' ]]; then
-    ./stop.sh
-    res="MAMP stopped."
-elif [[ "$1" = 'restart' ]]; then
-    ./stop.sh
-    sleep 1
-    ./start.sh
-    res="MAMP restarted."
-elif [[ "$1" = 'isrunning' ]]; then
-    hres="Apache is not running."
-    mres="Mysql is not running."
-    if [[ $(ps -ec | grep -c "httpd") -gt 0 ]]; then 
-        hres="Apache is running."
-    fi
-    if [[ $(ps -ec | grep -c "mysqld") -gt 0 ]]; then
-        mres="Mysql is running."
-    fi
-    res="$hres\n$mres"
-else
-    res="ERROR. Usage: mamp.sh <start|stop|restart|isrunning>"
-fi 
+case "$1" in
+    "start" )
+        ./start.sh
+        ;;
+    "stop" )
+        ./stop.sh
+        ;;
+    "restart" )
+        ./stop.sh
+        sleep 1
+        ./start.sh
+        ;;
+    "isrunning" )
+        [[ $(ps -ec | grep -c "httpd") -gt 0 ]] && echo "Apache is running." || echo "Apache is not running."
+        [[ $(ps -ec | grep -c "mysqld") -gt 0 ]] && echo "MySQL is running." || echo "MySQL is not running."
+        ;;
+    * )
+        echo "ERROR. Usage: mamp.sh <start|stop|restart|isrunning>"
+        ;;
+esac
 popd > /dev/null
-echo -e "$res"
