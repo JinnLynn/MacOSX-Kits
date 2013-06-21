1b2488b4d8387a568a0908692c33e0d83fc41c00
JinnLynn <eatfishlin@gmail.com>
2013-06-21 12:46:27 +0800

    cron统一

--- shell/cron.sh
+++ shell/cron.sh
@@ -8,3 +8,13 @@
 if [[ -z "$SSH_AUTH_SOCK" ]]; then
     export SSH_AUTH_SOCK=`find /tmp/ -path '*launch-*' -name 'Listeners' -print 2>/dev/null`
 fi
+
+# 备份
+function kits_cron_backup() {
+    kits.backup --quiet --no-exact-progress
+}
+
+# 整点报时
+function kits_cron_hourlynotify {
+    python $KITS/python/funcs.py hourlyNotify
+}
