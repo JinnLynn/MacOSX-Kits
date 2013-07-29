00df85f2bd59bf3db01ba29138ca9b4cff7cdba1
JinnLynn <eatfishlin@gmail.com>
2013-07-29 15:15:01 +0800

    genpac 更新

--- shell/sub/pac.sh
+++ shell/sub/pac.sh
@@ -1,6 +1,6 @@
 # 使用GenPAC生成自动代理配置文件
 function kits_pac_gen() {
-    python $KITS/extra/genpac/genpac.py
+    genpac --config-from=$KITS/cfg/genpac-config.ini --verbose
 }
 
 # 生成pac并更新
@@ -9,10 +9,13 @@ function kits_pac_update() {
     gist_repo=/Users/JinnLynn/Developer/Misc/Gist/5001700
     # push到gist要求的改变数量
     push_changed=10
-    # 生成
-    kits_pac_gen
+    personal_pac=~/.kits/tmp/pac.js
+    # 生成 一个人使用，包括自定义规则的pac文件
+    genpac --config-from=$KITS/cfg/genpac-config.ini --output=$personal_pac
     # 上传服务器
-    scp $gist_repo/pac.js $JPAC_SERVER
+    scp $personal_pac $JPAC_SERVER
+    # 生成 干净的仅包含gfwlist规则的pac文件
+    genpac --config-from=$KITS/cfg/genpac-config.ini --output=$gist_repo/pac.js --user-rule-from=''
     pushd $gist_repo > /dev/null
     # 当改变达到一定数量时自动push到gist
     if [[ $(git diff --numstat pac.js | awk '{print $1}') -gt $push_changed ]]; then 
@@ -21,4 +24,10 @@ function kits_pac_update() {
         echo 'gist updated.'
     fi
     popd > /dev/null
+}
+
+# 使新的pac立即生效
+function kits_pac_effective_immediately() {
+    # 改变pac地址路径即可
+    sudo networksetup -setautoproxyurl "Wi-Fi" "$JPAC_URL?$(date +%s)"
 }
