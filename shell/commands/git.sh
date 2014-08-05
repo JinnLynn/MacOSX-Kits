d44a7a356073e6d5e13b6f6418659c2e809ee1e3
JinnLynn <eatfishlin@gmail.com>
2014-08-05 16:46:30 +0800

    git default .gitignore update

--- shell/commands/git.sh
+++ shell/commands/git.sh
@@ -1,16 +1,39 @@
-# 默认`.gitignore`文件内容
-DEF_GITIGNORE=$(cat <<EOF
-# Windows
-[Dd]esktop.ini
-[Tt]humbs.db
-\$RECYCLE.BIN/
+# 生成.gitignore文件
+kits_git_ignore() {
+    local def_gitignore=$(cat <<EOF
+# Personal
+*.log
+tmp/
 
-# MacOSX
+# Python
+*.py[cod]
+
+# Windows
+# REF: https://github.com/github/gitignore/blob/master/Global/Windows.gitignore
+Desktop.ini
+Thumbs.db
+ehthumbs.db
+$RECYCLE.BIN/
+
+# MacOSX 
+# REF: https://github.com/github/gitignore/blob/master/Global/OSX.gitignore
 .DS_Store
 ._*
 .Spotlight-V100
 .Trashes
+.AppleDouble
+.LSOverride
+.AppleDB
+.AppleDesktop
+Network Trash Folder
+Temporary Items
+Icon
+
+
+#!!! Icon must end with two \r
 EOF)
+    echo -e "$def_gitignore" > .gitignore
+}
 
 # 在本地创建新的git repo
 kits_git_new_repo() {
@@ -19,7 +42,7 @@ kits_git_new_repo() {
     git init $1
     cd $1
     echo $1 > README.md
-    echo -e "$DEF_GITIGNORE" > .gitignore
+    kits_git_ignore
     git add .
     git commit -m 'first commit.'
 }
@@ -29,17 +52,14 @@ kits_git_new_server_repo() {
     [[ -z "$1" ]] && echo "ERROR. USAGE: <reponame>" && return
     # 确保是以.git结尾
     local reponame=$1
-    if [ ! -z "${reponame##*\.git}" ]; then
-        reponame="$reponame.git"
-    fi
+    [[ ! -z "${reponame##*\.git}" ]] && reponame="$reponame.git"
 
-    ssh scm@$JHOST <<EOF
+    # REF: http://stackoverflow.com/questions/7114990/pseudo-terminal-will-not-be-allocated-because-stdin-is-not-a-terminal
+    ssh -t -t -p $JHOME_SSH_PORT scm@$JHOME <<EOF
     cd /git
-    if [ -d "$reponame" ]; then
-        echo "ERROR. '$reponame' existed."
-        exit
-    fi
+    [[ -d "$reponame" ]] && echo "ERROR. '$reponame' existed." && exit
     git init --bare $reponame
+    exit
 EOF
 }
 
