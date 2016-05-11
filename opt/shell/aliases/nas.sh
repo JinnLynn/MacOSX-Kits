aef992680bcce4c3fc873f9e0e1fe3c001f93d5c
JinnLynn <eatfishlin@gmail.com>
2016-05-11 09:02:36 +0800

    nas.kits.update避免var目录被删除

--- opt/shell/aliases/nas.sh
+++ opt/shell/aliases/nas.sh
@@ -1,6 +1,7 @@
 # NAS相关
 
-alias nas.kits.update="kits.sync --protocol=30 --rsync-path /usr/bin/rsync -e 'ssh -p $JHOME_SSH_PORT -i $JKEY' $NASKITS/ root@$JHOME:/volume1/cellar/kits/"
+# 不能使用kits.sync(即不使用参数--delete-excluded) 避免var目录被删除
+alias nas.kits.update="rsync -avh --force --delete --ignore-errors --progress --stats --exclude-from='$KITS/etc/rsync-exclude.lst' --protocol=30 --rsync-path /usr/bin/rsync -e 'ssh -p $JHOME_SSH_PORT -i $JKEY' --exclude='/var' $NASKITS/ root@$JHOME:/volume1/cellar/kits/"
 alias nas.ip="ssh.home 'curl -s http://ip.3322.net'"
 alias nas.ip.host="dig +short $JHOST"
 
